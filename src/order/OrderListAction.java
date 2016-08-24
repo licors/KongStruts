f129ebb15d7eeecbc3eb698163dbda366036daa8
KH2\user2 <bebey7410@naver.com>
2016-08-24 17:10:59 +0900

    no message

---
+++ src/order/OrderListAction.java
@@ -0,0 +1,20 @@
+package order;
+
+import java.io.Reader;
+
+import com.ibatis.common.resources.Resources;
+import com.ibatis.sqlmap.client.SqlMapClient;
+import com.ibatis.sqlmap.client.SqlMapClientBuilder;
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+
+import admin.member.MemberVO;
+
+public class OrderListAction extends ActionSupport{
+	public static Reader reader;
+	public static SqlMapClient sqlMapper;
+	
+	private 
+	
+
+}
