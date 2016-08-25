c938396d9e99b3e3a192742505bce741009712e7
KH2\user2 <bebey7410@naver.com>
2016-08-25 21:29:44 +0900

    주문처리 액션 수정중

--- src/order/OrderProcessAction.java
+++ src/order/OrderProcessAction.java
@@ -1,5 +1,53 @@
 package order;
 
-public class OrderProcessAction {
+import java.io.IOException;
+import java.io.Reader;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
+import admin.showcase.showVO;
+import member.MemberVO;
+import basket.BasketVO;
+
+import com.ibatis.common.resources.Resources;
+import com.ibatis.sqlmap.client.SqlMapClient;
+import com.ibatis.sqlmap.client.SqlMapClientBuilder;
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class OrderProcessAction extends ActionSupport {
+/*	public static Reader reader;
+	public static SqlMapClient sqlMapper;
+
+	private MemberVO memparamClass;
+	private MemberVO memresultClass;
+	private showVO show_paramClass;
+	private showVO show_resultClass;
+	private OrderVO order_paramClass;
+	private OrderVO order_resultClass;
+	private BasketVO paramBas;
+	private BasketVO resultBas;
+	
+	private int order_num;	//주문번호
+	private int showboard_num;	//전시회 번호
+	private String subject;	//전시회 명
+	private int member_num;	//주문자 번호
+	private String name;	//주문자 이름
+	private String sex;		//주문자 성별
+	private String company;	//주문자 회사
+	private String address;
+	private String email;
+	private String qrcode;
+	private Date order_date;
+	private String tel;
+	private int status;
+	private String file_orgname;
+	private*/
+	
+	
+	
 }
