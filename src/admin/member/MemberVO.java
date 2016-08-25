1c5c4a49db55ea2f9c3098b77910e2ff339fd31e
KH2\user2 <bebey7410@naver.com>
2016-08-25 17:30:29 +0900

    memberVO 수정

--- src/admin/member/MemberVO.java
+++ src/admin/member/MemberVO.java
@@ -1,60 +1,18 @@
 package admin.member;
 
-public class MemberVO {
-	private String id;
-	private String passwd;
-	private String passwd2;
-	private String name;
-	private String nick;
-	private String sex;
-	private String birth;
+public class MemberVO {	
+	private int member_num;
 	private String email;
-	private String phone;
-	private String addr;
-	private String linum;
-	private String lipic;
+	private String password;
+	private String name;
+	private String address;
+	private String company;
 	
-	public String getId() {
-		return id;
+	public int getMember_num() {
+		return member_num;
 	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getPasswd() {
-		return passwd;
-	}
-	public void setPasswd(String passwd) {
-		this.passwd = passwd;
-	}
-	public String getPasswd2() {
-		return passwd2;
-	}
-	public void setPasswd2(String passwd2) {
-		this.passwd2 = passwd2;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getNick() {
-		return nick;
-	}
-	public void setNick(String nick) {
-		this.nick = nick;
-	}
-	public String getSex() {
-		return sex;
-	}
-	public void setSex(String sex) {
-		this.sex = sex;
-	}
-	public String getBirth() {
-		return birth;
-	}
-	public void setBirth(String birth) {
-		this.birth = birth;
+	public void setMember_num(int member_num) {
+		this.member_num = member_num;
 	}
 	public String getEmail() {
 		return email;
@@ -62,29 +20,28 @@ public class MemberVO {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	public String getPhone() {
-		return phone;
+	public String getPassword() {
+		return password;
 	}
-	public void setPhone(String phone) {
-		this.phone = phone;
+	public void setPassword(String password) {
+		this.password = password;
 	}
-	public String getAddr() {
-		return addr;
+	public String getName() {
+		return name;
 	}
-	public void setAddr(String addr) {
-		this.addr = addr;
+	public void setName(String name) {
+		this.name = name;
 	}
-	public String getLinum() {
-		return linum;
+	public String getAddress() {
+		return address;
 	}
-	public void setLinum(String linum) {
-		this.linum = linum;
+	public void setAddress(String address) {
+		this.address = address;
 	}
-	public String getLipic() {
-		return lipic;
+	public String getCompany() {
+		return company;
 	}
-	public void setLipic(String lipic) {
-		this.lipic = lipic;
+	public void setCompany(String company) {
+		this.company = company;
 	}
-	
 }
