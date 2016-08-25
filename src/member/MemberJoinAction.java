b663c194b2f94689a99f2e66aba4a310ee11a634
KimYoungGil <zersking@naver.com>
2016-08-25 18:09:21 +0900

    memberSQL.xml추가, member회원가입 클래스 완성

---
+++ src/member/MemberJoinAction.java
@@ -0,0 +1,81 @@
+package member;
+
+import java.io.Reader;
+
+import com.ibatis.common.resources.Resources;
+import com.ibatis.sqlmap.client.SqlMapClient;
+import com.ibatis.sqlmap.client.SqlMapClientBuilder;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class MemberJoinAction extends ActionSupport {
+	
+	public static Reader reader; // 파일 스트림을 위한 reader
+	public static SqlMapClient sqlMapper;// SqlMapClient API를 사용하기 위함
+	private MemberVO memresultClass;
+	private MemberVO memparamClass;
+	
+	
+	
+	private String email;
+	private String passwd;
+	
+	public MemberJoinAction() throws Exception {
+		// sqlMapConfig.xml 파일의 설정 내용을 가져온다
+		reader = Resources.getResourceAsReader("sqlMapConfig.xml");
+		// sqlMapConfig.xml 의 내용을 적용한 sqlMapper 객체 생성
+		sqlMapper = SqlMapClientBuilder.buildSqlMapClient(reader);
+		reader.close();
+	}
+	
+	public String execute() throws Exception {
+		memparamClass = new MemberVO();
+		memresultClass = new MemberVO();
+		
+		
+		memparamClass.setEmail(getEmail());
+		memparamClass.setPasswd(getPasswd());
+		
+		
+		sqlMapper.insert("joinMember", memparamClass);
+		memresultClass=(MemberVO)sqlMapper.queryForObject("UserCheck",memparamClass.getEmail());
+		
+		return SUCCESS;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPasswd() {
+		return passwd;
+	}
+
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+
+	public MemberVO getMemresultClass() {
+		return memresultClass;
+	}
+
+	public void setMemresultClass(MemberVO memresultClass) {
+		this.memresultClass = memresultClass;
+	}
+
+	public MemberVO getMemparamClass() {
+		return memparamClass;
+	}
+
+	public void setMemparamClass(MemberVO memparamClass) {
+		this.memparamClass = memparamClass;
+	}
+	
+	
+	
+
+
+}
---
+++ src/member/MemberVO.java
@@ -0,0 +1,51 @@
+package member;
+
+public class MemberVO {
+	
+	private String member_num;
+	private String email;
+	private String passwd;
+	private String name;
+	private String address;
+	private String company;
+	
+	public String getMember_num() {
+		return member_num;
+	}
+	public void setMember_num(String member_num) {
+		this.member_num = member_num;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPasswd() {
+		return passwd;
+	}
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	
+	
+
+}
