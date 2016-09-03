5038a425ff361bd215a40d3d8f731e2bc57543c4
Silvi <lilin@nyang.kr>
2016-09-03 16:25:11 +0900

    EEEEEEEEEEE-mail 받기 구현 문제는 서버 켜질때 한번만 받음... 쓸모가...스케쥴러로 5분마다 받아야하나

--- src/admin/support/email/support_email.java
+++ src/admin/support/email/support_email.java
@@ -20,7 +20,7 @@ import com.sun.mail.pop3.POP3Store;
 import java.io.IOException;
 import javax.mail.Folder;
 import javax.mail.NoSuchProviderException;
-import java.util.StringTokenizer;
+import javax.mail.internet.MimeMultipart;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -30,8 +30,13 @@ public class support_email extends HttpServlet {
     // 환경 설정 변수
     private String contentType = "text/html";
     private String endcoding = "UTF-8";
-    final String username = "server@nyang.kr";
-    final String password = "ilove2silvi";
+
+    private final String username = "server@nyang.kr";
+    private final String password = "ilove2silvi";
+
+    private static final String smtp = "smtp.worksmobile.com";
+    private static final String pop = "pop.worksmobile.com";
+
     private static final support_email instance = new support_email();
     private static Properties props;
 
@@ -43,8 +48,8 @@ public class support_email extends HttpServlet {
         props = System.getProperties();
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.transport.protocol","smtp" );
-        props.put("mail.smtp.host", "smtp.worksmobile.com");
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.host", smtp);
         props.put("mail.smtp.port", "587");
     }
 
@@ -54,12 +59,9 @@ public class support_email extends HttpServlet {
         getInstance().received();
     }
 
-//MailSender.getInstance().send("", "bcnsmaster@gmail.com", getName(), msg);//보내는사람,받는사람,보내는사람이름,제목,내용
+    //보내는사람,받는사람,보내는사람이름,제목,내용
     public void send(String from, String to, String name, String subjectz, String content) {
         try {
-            // SMTP 설정
-
-            // Session, MimeMessage 생성
             Session sess = Session.getInstance(props,
                     new javax.mail.Authenticator() {
                 @Override
@@ -83,58 +85,47 @@ public class support_email extends HttpServlet {
             // 전송
             Transport.send(msg);
         } catch (AddressException ae) {
-            System.out.println("[Error : " + ae.getMessage() + "]");
+            System.out.println("Error : " + ae.getMessage());
             ae.printStackTrace(System.err);
         } catch (MessagingException me) {
-            System.out.println("[Error : " + me.getMessage() + "]");
+            System.out.println("Error : " + me.getMessage());
             me.printStackTrace(System.err);
         } catch (UnsupportedEncodingException ue) {
-            System.out.println("[Error : " + ue.getMessage() + "]");
+            System.out.println("Error : " + ue.getMessage());
             ue.printStackTrace(System.err);
         }
     }
 
-    //receiveEmail(String pop3Host, String storeType, String user, String password) {
     public void received() {
         try {
+            Properties properties = new Properties();
+            properties.put("mail.pop3.host", pop);
+            Session emailSession = Session.getDefaultInstance(properties);
 
-//            String mailPop3Host = "cakelycakes.com";
-//            String mailStoreType = "pop3";
-//            String mailUser = "bigcakes";
-//            String mailPassword = "bigcakes";
-//            Properties properties = new Properties();
-//            properties.put("mail.pop3.host", pop3Host);
-//            Session sess = Session.getDefaultInstance(props);
-            System.out.println("props" + props.toString());
-            Session sess = Session.getInstance(props,
-                    new javax.mail.Authenticator() {
-                @Override
-                protected PasswordAuthentication getPasswordAuthentication() {
-                    return new PasswordAuthentication(username, password);
-                }
-            });
+            POP3Store emailStore = (POP3Store) emailSession.getStore("pop3");
+            emailStore.connect(username, password);
 
-            POP3Store emailStore = (POP3Store) sess.getStore("pop3");
-            emailStore.connect();
-            System.out.println("e:" + emailStore.toString());
             Folder emailFolder = emailStore.getFolder("INBOX");
-            if (emailFolder.hasNewMessages()) {
-                System.out.println("메일옴!");
-            }
             emailFolder.open(Folder.READ_ONLY);
 
             Message[] messages = emailFolder.getMessages();
-            for (int i = 0; i < messages.length; i++) {
-                Message message = messages[i];
-                System.out.println("==============================");
-                System.out.println("Email #" + (i + 1));
-                System.out.println("Subject: " + message.getSubject());
-                System.out.println("From: " + message.getFrom()[0]);
-                System.out.println("Text: " + message.getContent().toString());
+
+            Message message = messages[messages.length - 1]; //가장 마지막에 온 메일(문의)
+
+            System.out.println("Subject: " + message.getSubject()); //제목 = 문의 type
+            System.out.println("From: " + message.getFrom()[0]); //보낸사람 = member e-mail
+
+            if (message.getContent() instanceof MimeMultipart) {
+                MimeMultipart mimeMultipart = (MimeMultipart) message.getContent();
+//                System.out.println("type: " + mimeMultipart.getBodyPart(0).getContentType() + ":");
+                System.out.println("Text:" + mimeMultipart.getBodyPart(0).getContent()); //문의 내용
+            } else {
+                System.out.println();
+                System.out.println("Text:" + message.getContent()); //문의 내용
             }
 
             emailFolder.close(false);
-//            emailStore.close();
+            emailStore.close();
         } catch (NoSuchProviderException e) {
             e.printStackTrace();
         } catch (MessagingException e) {
