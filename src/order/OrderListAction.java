1001fd4b3f56a04d30ea725a4b9f4fd1a7e21740
yujin <bebey7410@naver.com>
2016-09-03 23:21:04 +0900

    관리자용 orderList 임시완성 (검색기능 한글 깨져서 숫자나 영어만 검색 가능), 달력으로 기간검색 기능추가 예정

--- src/order/OrderListAction.java
+++ src/order/OrderListAction.java
@@ -1,5 +1,6 @@
 package order;
 
+import order.AdminOrderPagingAction;
 import java.io.IOException;
 import java.io.Reader;
 import java.util.ArrayList;
@@ -35,8 +36,10 @@ public class OrderListAction extends ActionSupport {
     private int num = 0;
     private int member_num;
 
+    //검색관련
     private String searchKeyword;
-    private List<OrderVO> list = new ArrayList<OrderVO>();
+    private int searchNum;
+    //private int num2 = 0;
 
     public OrderListAction() throws IOException {
         reader = Resources.getResourceAsReader("sqlMapConfig.xml");
@@ -73,6 +76,7 @@ public class OrderListAction extends ActionSupport {
         return SUCCESS;
     }
     
+    //관리자용 회원 오더리스트(페이징, 검색)
     public String adminOrderList() throws Exception {
     	memparamClass = new MemberVO();
         memresultClass = new MemberVO();
@@ -89,17 +93,53 @@ public class OrderListAction extends ActionSupport {
 
         totalCount = orderList.size();
 
-        page = new OrderPagingAction(currentPage, totalCount, blockCount, blockPage, num, "");
-        pagingHtml = page.getPagingHtml().toString();
+        adpage = new AdminOrderPagingAction(currentPage, totalCount, blockCount, blockPage, num, "");
+        pagingHtml = adpage.getPagingHtml().toString();
         int lastCount = totalCount;
 
-        if (page.getEndCount() < totalCount) {
-            lastCount = page.getEndCount() + 1;
+        if (adpage.getEndCount() < totalCount) {
+            lastCount = adpage.getEndCount() + 1;
         }
 
-        orderList = orderList.subList(page.getStartCount(), lastCount);
+        orderList = orderList.subList(adpage.getStartCount(), lastCount);
     	return SUCCESS;
     }
+    
+    public String search() throws Exception {
+    	memresultClass = admin.MemberLoginCheck.getMember(sqlMapper, memresultClass);
+
+		searchKeyword = new String(searchKeyword.getBytes("8859_1"), "utf-8");
+		System.out.print(searchKeyword);
+
+		if (searchNum == 0) {	//회원
+			orderList = sqlMapper.queryForList("order.search_id", "%" + getSearchKeyword() + "%");
+		}
+		if (searchNum == 1) {	//전시
+			orderList = sqlMapper.queryForList("order.search_subject", "%" + getSearchKeyword() + "%");
+		}
+		if (searchNum == 2) {	//신청상태
+			orderList = sqlMapper.queryForList("order.search_status", "%" + getSearchKeyword() + "%");
+		}
+		/*if (searchNum == 2) {	//날짜
+		orderList = sqlMapper.queryForList("order.search_date", "%" + getSearchKeyword() + "%");
+	}*/
+
+		totalCount = orderList.size(); // 전체 글 갯수를 구한다.
+		// pagingAction 객체생성
+		adpage = new AdminOrderPagingAction(currentPage, totalCount, blockCount, blockPage, searchNum, getSearchKeyword());
+		pagingHtml = adpage.getPagingHtml().toString(); // 페이지HTML 생성.
+
+		// 현재 페이지에서 보여줄 마지막 글의 번호 설정.
+		int lastCount = totalCount;
+
+		// 현재 페이지의 마지막 글의 번호가 전체의 마지막 글 번호보다 작으면 lastCount를 +1 번호로 설정.
+		if (adpage.getEndCount() < totalCount)
+			lastCount = adpage.getEndCount() + 1;
+
+		// 전체 리스트에서 현재 페이지만큼의 리스트만 가져온다.
+		orderList = orderList.subList(adpage.getStartCount(), lastCount);
+		return SUCCESS;
+	}
 
     public String getSearchKeyword() {
         return searchKeyword;
@@ -228,5 +268,29 @@ public class OrderListAction extends ActionSupport {
     public void setMember_num(int member_num) {
         this.member_num = member_num;
     }
+
+	public AdminOrderPagingAction getAdpage() {
+		return adpage;
+	}
+
+	public void setAdpage(AdminOrderPagingAction adpage) {
+		this.adpage = adpage;
+	}
+
+	public String getAdpagingHtml() {
+		return adpagingHtml;
+	}
+
+	public void setAdpagingHtml(String adpagingHtml) {
+		this.adpagingHtml = adpagingHtml;
+	}
+
+	public int getSearchNum() {
+		return searchNum;
+	}
+
+	public void setSearchNum(int searchNum) {
+		this.searchNum = searchNum;
+	}
 
 }
